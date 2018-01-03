2d3dd902241aa7d2b7164f89fbb0c9277354c9ff
Andres0919 <andres.posada0919@gmail.com>
2018-01-03 16:25:47 -0500

    componentes react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import Welcome from './Welcome';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      title: "Hola mundo desde Estado"
+    }
+  }
   render() {
+    const names = ["Pedro","Juan","German"];
     return (
-    <h1>Hola Mundo con REACT!!!</h1>
+      <div>
+        {names.map(name =>
+        <Welcome key = {name} name = {name} />
+      )}
+      <h1>{this.state.title}</h1>
+      <button onClick={this.changeTitle.bind(this)} >Cambie el titulo </button>
+      </div>  
+      
     );
+  }
+  changeTitle(){
+    this.setState({
+      title: "Nuevo título"
+    });
   }
 }
 
